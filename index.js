f88b74ede1619b09bef4fe647ac25e77754bd6da
Zachary Walters <z.walters734@gmail.com>
2021-02-15 12:49:41 -0600

    Small update

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
 const MaterialDown = require('./src/MaterialDown');
-const React = require('react');
 
-const test = (string) => {
-  if (typeof string !== 'string') {
-    throw new TypeError('Input Error: Need String');
-  }
-
-  return (
-    <MaterialDown>
-      {string}
-    </MaterialDown>
-  );
-};
-
-
-console.log(test('Test Data'));
+module.exports = MaterialDown;
